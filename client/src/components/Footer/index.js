8e31c962976f6a688332aac28526614ae9aa84e1
Kimberly <kamueller2@gmail.com>
2019-07-31 22:33:49 -0400

    created components for page

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+      document.write(new Date().getFullYear());
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
