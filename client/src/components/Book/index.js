16671fe298841d7277799646c4eb7c00a6795b89
Kimberly <kamueller2@gmail.com>
2019-08-01 20:47:48 -0400

    fixed book func

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { ListItem } from "../List";
 import { Row, Col } from "../Grid";
-// import "./style.css";
 
-function Book({ title, subtitle, authors, link, description, image, Button }) {
+function Book ({ 
+  title, 
+  subtitle, 
+  authors,
+  link, 
+  description, 
+  image, 
+  Button 
+}) {
   return (
     <ListItem>
       <Row className="flex-wrap-reverse">
